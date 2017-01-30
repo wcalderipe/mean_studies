cef00e0638e92e36a65527f173a55a5046a635fa
igor.quevedo <igor.quevedo@nodo.cc>
2017-01-30 15:58:18 -0200

    models directory loaded into app and first model/schema created

---
+++ app/models/foto.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({
+	titulo: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	grupo: {
+		type: Number,
+		required: true
+	}
+});
+
+var Foto = mongoose.model('Foto', schema);
+
+module.exports = Foto;
