1c480657d34b93e91d4c6f92eab9ecb0b4205978
igor.quevedo <igor.quevedo@nodo.cc>
2017-02-09 09:21:27 -0200

    Token sign, verification and authentication implemented

--- public/js/services/token-interceptor.js
+++ public/js/services/token-interceptor.js
@@ -1,23 +1,44 @@
 angular.module('alurapic')
-	.factory('tokenInterceptor', function($window){
+	.factory('tokenInterceptor', function($window, $q, $location){
 
 		var interceptor = {};
 
 		interceptor.response = function(response) {
-			console.log('Response received');
 
 			var token = response.headers('x-access-token');
 
 			if(token) {
-				console.log('Token accepted');
 				
 				// Client-side data persistance. This keeps the token into the browser session storage.
 				// The token is deleted when the current tab is closed.
 
 				$window.sessionStorage.token = token;
+				console.log('Token stored into the browser');
 			}
 
 			return response;
+		};
+
+		interceptor.request = function(config) {
+			
+			config.headers = config.headers || {};
+
+			if($window.sessionStorage.token) {
+				config.headers['x-access-token'] = $window.sessionStorage.token;
+				console.log('Token added into request headers');
+			}
+			return config;
+		}
+
+		interceptor.responseError = function(rejection) {
+			
+			if(rejection != null && rejection.status == 401){
+				// Delete the session storage cached token and redirects the user to login page if the status sent via response is 401(unauthorized);
+				delete $window.sessionStorage.token;
+				$location.path('/login');
+			}
+
+			return $q.reject(rejection);
 		};
 
 		return interceptor;
